703863bf46205a997920e0d1bd2a1ba5e7865b9a
Dhwani <dhwanijain731@gmail.com>
2025-11-05 20:21:38 +0530

    added app.py file

--- app.py
+++ app.py
@@ -1,5 +1,32 @@
-import pandas as pd
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
 
-df = pd.read_csv("Dataset/iris.csv")
-print(df.head())
-print(df.columns)
+# Load model and encoder
+model = joblib.load("model.pkl")
+label_encoder = joblib.load("label_encoder.pkl")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message": "Iris ML API is running!"}
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json  # Get JSON input
+
+    features = np.array([[
+        data["sepal_length"],
+        data["sepal_width"],
+        data["petal_length"],
+        data["petal_width"]
+    ]])
+
+    prediction = model.predict(features)[0]
+    decoded_prediction = label_encoder.inverse_transform([prediction])[0]
+
+    return jsonify({"prediction": decoded_prediction})
+
+if __name__ == "__main__":
+    app.run(debug=True)
