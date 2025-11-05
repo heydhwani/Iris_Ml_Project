1ff5589b5394bef6f9594e97de3ddfa12e1d2491
Dhwani <dhwanijain731@gmail.com>
2025-11-05 19:02:50 +0530

    splitted data into training and testing

--- train_model.py
+++ train_model.py
@@ -14,3 +14,6 @@ y = df["species"]
 # Label Encode target column
 le = LabelEncoder()
 y_encoded = le.fit_transform(y)
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
