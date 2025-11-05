254989b7a3de48a7c30601aebd6df8aea3792dd7
Dhwani <dhwanijain731@gmail.com>
2025-11-05 19:12:28 +0530

    trained model with help of randomforestclassifier

--- train_model.py
+++ train_model.py
@@ -17,3 +17,7 @@ y_encoded = le.fit_transform(y)
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
