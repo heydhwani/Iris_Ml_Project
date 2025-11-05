45c1c631aed8f14c2d6827bc28148ccdbeb8981e
Dhwani <dhwanijain731@gmail.com>
2025-11-05 18:47:22 +0530

    loaded dataset

--- train_model.py
+++ train_model.py
@@ -3,3 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+
+# Load dataset
+df = pd.read_csv("Dataset/iris.csv")
